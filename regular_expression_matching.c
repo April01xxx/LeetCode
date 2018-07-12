ba2094e9fe3160847c76f3927d58e317d86bc687
april01xxx <april01xxx@gmail.com>
2018-07-12 23:37:56 +0800

    wonderful solution about regular expression

--- regular_expression_matching.c
+++ regular_expression_matching.c
@@ -90,3 +90,56 @@ isMatch(char *s, char *p) {
   free(dp);
   return ret;
 }
+
+/*
+ * 一种优化的动态规划方案.
+ * 对于给定字符串s和匹配模式p,某个子串s[0:i]是否与当前模式匹配,有以下几种情况:
+ * 设reg表示已经匹配完成的模式,c表示当前要匹配的字符,n表示紧跟c之后的要匹配的
+ * 字符,ans[i + 1]表示子串s[0:i]是否与模式匹配,其中i+1表示子串的长度.
+ * 1. n为星号,这表示子串s[0:i]要与模式reg+c+*匹配,必须满足以下两种情况中的一种:
+ *    a. s[0:i]匹配模式reg(c匹配0次);
+ *    b. s[0:i-1]匹配模式reg且s[i]与c匹配(c匹配1次);
+ *    c. c出现多次的情况s[0:i]无法匹配;
+ * 故: ans[i + 1] = ans[i + 1] || (ans[i] && (s[i] == c || c == '.')).
+ * 注意到ans为bool类型,逻辑运算与位运算等价.
+ * 2. n不是星号,这表示在字符串s中必须存在一个子串能与该模式(reg+c)匹配(如不存在
+ *    这样的子串,则s与模式p不匹配),这个子串有以下两个特点:
+ *    a. 子串的最后一位与c匹配;
+ *    b. 子串除最后一位外与模式reg匹配;
+ *    采用从右向左遍历字符串s的方式寻找这样的子串.
+ */
+#define bool char
+#define true 1
+#define false 0
+
+bool
+isMatch(char *s, char *p) {
+  size_t len, i;
+  bool *ans, result;  /* 用ans[i + 1]来记录子串s[0:i]与模式是否匹配,s[0:0]表示空串 */
+
+  len = strlen(s);
+  ans = malloc((len + 1) * sizeof(bool));
+  memset(ans + 1, 0, len);
+  *ans = true;   /* ""(s[0:0]) matches "" */
+
+  for ( ; *p != 0; ++p) {
+    if (p[1] == '*') {
+      for (i = 0; i < len; ++i)
+        ans[i + 1] = ans[i + 1] | (ans[i] & (s[i] == *p || *p == '.'));
+      /* 跳过星号 */
+      ++p;
+    } else {
+      /* 从右往左搜索 */
+      for (i = 0; i < len; ++i)
+        ans[len - i] = ans[len - i - 1] & (s[len - i - 1] == *p || *p == '.');
+
+      /* 不存在一个字符后紧跟一个星号的模式的话,则无法匹配空串.
+       * 例如模式"ab*"无法匹配空串,而"a*"可以匹配空串.
+       */
+      ans[0] = false;
+    }
+  }
+  result = ans[len];
+  free(ans);
+  return result;
+}
