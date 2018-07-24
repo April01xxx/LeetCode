8fb7025af5860750bd5fc71eeea8cbc0ab42ef8e
Lancer Wong <lancer_cool@163.com>
2018-07-24 09:12:22 +0800

    O(2n)解法

--- longest_valid_parentheses.c
+++ longest_valid_parentheses.c
@@ -36,3 +36,50 @@ longestValidParentheses(char *s) {
 
   return max;
 }
+
+/**
+ * 上面的解法中,若出现open>close的情况,我们会回溯,然后再次寻找可能
+ * 的最长有效括号组合.回溯的过程中,若一直遇到open>close的情况,则整
+ * 个算法的效率会很低.我们注意到若最终open<=close,那么遍历完成后肯
+ * 定找到了最长有效括号组合,否则的话,我们只需要反向再遍历一次就可以
+ * 找到可能的最长组合.
+ */
+int
+longestValidParentheses(char *s) {
+  int open, close, max, i;
+
+  open = close = max = 0;
+  for (i = 0; s[i] != 0; ++i) {
+    if (s[i] == '(')
+      ++open;
+    else
+      ++close;
+
+    if (open < close)
+      open = close = 0;
+    else if (open == close) {
+      if (open + close > max)
+        max = open + close;
+    }
+  }
+
+  if (open <= close)
+    return max;
+
+  /* 一趟遍历完成后open>max,则反向遍历寻找可能最长的组合. */
+  open = close = 0;
+  for (i = strlen(s) - 1; i >= 0; --i) {
+    if (s[i] == ')')
+      ++open;
+    else
+      ++close;
+
+    if (open < close)
+      open = close = 0;
+    else if (open == close) {
+      if (open + close > max)
+        max = open + close;
+    }
+  }
+  return max;
+}
