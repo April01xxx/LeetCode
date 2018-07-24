db75e4a967d868cb437fdcd82522bcf342840ff4
april01xxx <april01xxx@gmail.com>
2018-07-25 07:20:42 +0800

    使用二维数组记录每个小方块内的元素

--- valid_sudoku.c
+++ valid_sudoku.c
@@ -10,21 +10,22 @@
  * 1. 用两个二维数组记录该行该列该元素是否存在: rows[9][10],cols[9][10].
  *    对于board中的某个元素board[i][j] = x,判断rows[i][x],cols[j][x]是否
  *    为0,若都为0表明行列的检查通过;
- * 2. 用一个一维数组记录3x3小方块内元素是否存在sub_box[10],若sub_box[x]为0表明
- *    sub_box检查通过;
+ * 2. 用一个二维数组记录3x3小方块内元素是否存在sub_box[9][10],若sub_box[k][x]
+ *    为0表明sub_box检查通过;
  * 3. 若以上两个条件均满足,则表明该元素有效.
  * 按照以上逻辑逐个检查已填充的元素,每个sub_box是一次完整的处理.
  */
 bool
 isValidSudoku(char **board, int boardRowSize, int boardColSize) {
-  int rows[9][10], cols[9][10], sub_box[10];
-  int row_step, col_step, i, j, c;
+  int rows[9][10], cols[9][10], sub_box[9][10];
+  int row_step, col_step, i, j, k, c;
 
   memset(rows, 0, 90 * sizeof(int));
   memset(cols, 0, 90 * sizeof(int));
+  memset(sub_box, 0, 90 * sizeof(int));
+  k = 0;
   for (row_step = 0; row_step < 9; row_step += 3) {
     for (col_step = 0; col_step < 9; col_step += 3) {
-      memset(sub_box, 0, 10 * sizeof(int));
       for (i = 0; i < 3; ++i) {
         for (j = 0; j < 3; ++j) {
           c = board[row_step + i][col_step + j];
@@ -34,12 +35,13 @@ isValidSudoku(char **board, int boardRowSize, int boardColSize) {
             continue;
           if (rows[row_step + i][c] != 0 ||
               cols[col_step + j][c] != 0 ||
-              sub_box[c] != 0)
+              sub_box[k][c] != 0)
             return 0;
           else
-            rows[row_step + i][c] = cols[col_step + j][c] = sub_box[c] = 1;
+            rows[row_step + i][c] = cols[col_step + j][c] = sub_box[k][c] = 1;
         }
       }
+      ++k;
     }
   }
   return 1;
