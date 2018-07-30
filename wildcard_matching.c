fb19f6cab2016a815ee4969e1f8cb4474e9e9583
april01xxx <april01xxx@gmail.com>
2018-07-31 00:23:41 +0800

    添加递归的解法

--- wildcard_matching.c
+++ wildcard_matching.c
@@ -41,7 +41,7 @@ isMatch(char *s, char *p) {
       /**
        * 当前p指向'*'匹配任意序列,假设之前的模式为reg,则现在整个模式为reg+'*',
        * 显然若从某个位置开始子串s[0:i]与reg匹配,那么对于任意的m>=i,子串s[0:m]
-       * 肯定与模式reg+'*'匹配.
+       * 肯定与模式reg+'*'匹配.这里可以做剪枝优化,跳过连续的星号.
        */
       for (i = 0; i < len; ++i)
         ans[i + 1] |= ans[i];
@@ -55,8 +55,38 @@ isMatch(char *s, char *p) {
        */
       ans[0] = false;
     }
+    /* 这里可以做剪枝优化,若ans全为false,那么最终结果肯定是false. */
   }
   result = ans[len];
   free(ans);
   return result;
+}
+
+/**
+ * 上面的解法,若字符串s的长度为N,模式p的长度为M,则整个算法的时间复杂度是O(NM).
+ * 下面采用递归的方式来求解,递归求解虽然复杂度高,但是更直观.
+ * 1. 若模式p的当前字符不是'*',则逐字符匹配即可;
+ * 2. 若模式p的当前字符是'*',分几种情况讨论:
+ *    a. s[0] != p[1], 直接比较s[1]与p[1];
+ *    b. s[0] == p[1], 匹配s[0]和p[1];
+ * 此方法在LeetCode上会TLE.
+ */
+bool
+isMatch(char *s, char *p) {
+  size_t len1, len2;
+
+  if (s == NULL)
+    return false;
+
+  len1 = strlen(s);
+  len2 = strlen(p);
+
+  if (len2 == 0)
+    return len1 == 0;
+
+  if (p[0] == '*')
+    return ((s[0] == p[1] || p[1] == '?' || p[1] == '*') && isMatch(s, p + 1)) ||
+           (len1 > 0 && isMatch(s + 1, p));
+  else
+    return len1 > 0 && (s[0] == p[0] || p[0] == '?') && isMatch(s + 1, p + 1);
 }
