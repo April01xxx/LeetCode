f5f7e9c6fdd2850469fc20eff3536a9a21c8c7fd
Lancer Wong <lancer_cool@163.com>
2018-08-20 11:11:46 +0800

    添加动态规划解法

--- scramble_string.c
+++ scramble_string.c
@@ -20,7 +20,7 @@
  *
  * 每次遇到字符串的题都有点懵,没啥编程思路,看了tag是dp,想了很久也没想出来要dp的
  * 是啥.先试试brute force吧,如果s2是s1的一个扰乱字符串,那么必定存在一个分割点p,
- * 使得s2[0:p]或者s2[p+1:len]是s1[0:p]的一个扰乱字符串.递归处理每个子串,最终得
+ * 使得s2[0:p]或者s2[len-p:len]是s1[0:p]的一个扰乱字符串.递归处理每个子串,最终得
  * 到结果.PS: 居然AC了而且beats 100%...可能测试案例太少.有点迷.
  */
 bool
@@ -44,10 +44,10 @@ isScramble(char *s1, char *s2) {
   for (i = 0; i < len - 1; ++i) {
     int target1[26] = {0}, target2[26] = {0};
 
-    ++dict[s1[i]-'a'];
+    ++dict[s1[i] - 'a'];
 
     for (j = 0; j <= i; ++j)
-      ++target1[s2[j]-'a'];
+      ++target1[s2[j] - 'a'];
 
     flag1 = true;
     for (k = 0; k < 26; ++k) {
@@ -58,7 +58,7 @@ isScramble(char *s1, char *s2) {
     }
 
     for (j = len - 1; j >= len - 1 - i; --j)
-      ++target2[s2[j]-'a'];
+      ++target2[s2[j] - 'a'];
 
     flag2 = true;
     for (k = 0; k < 26; ++k) {
@@ -89,7 +89,7 @@ isScramble(char *s1, char *s2) {
 
       c1 = s1[i + 1];
       s1[i + 1] = 0;
-      flag1 = isScramble(s1, &s2[len - 1 -i]);
+      flag1 = isScramble(s1, &s2[len - 1 - i]);
 
       s1[i + 1] = c1;
       c2 = s2[len - i - 1];
@@ -103,4 +103,74 @@ isScramble(char *s1, char *s2) {
   }
 
   return false;
+}
+
+/**
+ * 上面的解法是递归,不过在递归之前先判断分割后的字符串是否是anagram,如果是才继续
+ * 递归处理.上面提到这道题可以用dp来做,看了下讨论区别人的解法,现记录在此.
+ * 我们维护三个变量:i,j,l.其中i是s1的起始下标,j是s2的起始下标,l是子串的长度.
+ * 那么dp[i][j][len]表示s1中以i为起始下标长度为l的子串与s2中以j为起始下标长度为l
+ * 的子串是否是scramble string,那最终的问题就是要求解dp[0][0][len].那么状态转移
+ * 方程是什么呢?根据题意,一个长度为l的字符串存在l-1种分割的方法,只要其中有一种满足
+ * scramble string的性质,那么dp[i][j][l]的结果就是true.也就是说对于每一个k,其中
+ * 1<=k<=l,如果:
+ *                   (dp[i][j][k] && dp[i+k][j+k][l-k])
+ * 或者
+ *                   (dp[i][j+l-k][k] && dp[i+l-k][j][l-k])
+ * 两者任意一个为真则dp[i][j][l]为真.
+ *                   i                   i+l
+ * s1:               aaaaaaaaaaaaaaaaaaaa
+ * s2:               bbbbbbbbbbbbbbbbbbbb
+ *                            k
+ * 示意图如上,对于一种分割方法k,存在两种可能:
+ * 1. s1的左半部分和s2的左半部分是scrambled且s1的右半部分和s2的右半部分是scrambled;
+ * 2. s1的左半部分和s2的右半部分是scrambled且s1的右半部分和s2的左半部分是scrambled;
+ * 状态转移方程有了,那么初始值是什么呢?如果长度为1,那显然当s1[0]==s2[0]时为真.
+ * 这里为了编程方面,长度取字符串长度加1.
+ */
+bool
+isScramble(char *s1, char *s2) {
+  bool ***dp, ans;
+  int i, j, k, l, len;
+
+  len = strlen(s1);
+  dp = (bool ***)malloc(len * sizeof(bool **));
+  for (i = 0; i < len; ++i) {
+    dp[i] = (bool **)malloc(len * sizeof(bool *));
+    for (j = 0; j < len; ++j)
+      dp[i][j] = (bool *)calloc(1 + len, sizeof(bool));
+  }
+
+  /* 计算初始值. */
+  for (i = 0; i < len; ++i) {
+    for (j = 0; j < len; ++j)
+      dp[i][j][1] = (s1[i] == s2[j]);
+  }
+
+  /**
+   * 这里有几点要注意:
+   * 1. l为1的情况已经计算过了,这里从2开始循环处理.
+   * 2. i,j表示的是s1,s2中子串的起始下标.
+   * 3. 每次处理长度为l的子串,故i,j的取值范围都是[0,len-l+1].
+   * 4. l-1种分割方法种,任意一种为真即可,这里可以进行剪枝优化,判断dp[i][j][l]
+   *    为真了就可以退出最内层循环.
+   */
+  for (l = 2; l <= len; ++l) {
+    for (i = 0; i <= len - l; ++i) {
+      for (j = 0; j <= len - l; ++j) {
+        for (k = 1; k < l; ++k)
+          dp[i][j][l] |= (dp[i][j][k] && dp[i + k][j + k][l - k]) ||
+                        (dp[i][j + l - k][k] && dp[i + k][j][l - k]);
+      }
+    }
+  }
+  ans = dp[0][0][len];
+  for (i = 0; i < len; ++i) {
+    for (j = 0; j < len; ++j)
+      free(dp[i][j]);
+    free(dp[i]);
+  }
+  free(dp);
+
+  return ans;
 }
