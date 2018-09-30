b1bc573c129cce48e042626288d2db9ba639f410
Lancer Wong <lancer_cool@163.com>
2018-09-30 09:06:58 +0800

    find minimum in rotated sorted array

--- find_minimum_in_rotated_sorted_array.c
+++ find_minimum_in_rotated_sorted_array.c
@@ -14,7 +14,7 @@
  * Output: 0
  *
  * 找出数组中的最小元素.
- * 这道题的难度居然是medium,有点无法理解,出题者的意图应该是要求时间复杂度为O(nlogn)吧.
+ * 这道题的难度居然是medium,有点无法理解,出题者的意图应该是要求时间复杂度为O(logn)吧.
  * 求一个数组中的最小值,最直观的做法是遍历整个数组.
  */
 int
@@ -27,4 +27,31 @@ findMin(int *nums, int numsSize) {
   }
 
   return ans;
+}
+
+
+/**
+ * 原数组是已经排序的,只不过在某个位置旋转了,仍可以利用二分查找的方法来寻找
+ * 最小元素.以[4,5,6,7,0,1,2]为例:
+ *    初始时lo=0,hi=6,mid=3;因为nums[lo] > nums[hi],故nums[0]不是所求的
+ *    最小元素.又因为nums[mid] > nums[hi],故可知所求的元素在[mid,hi]区间
+ *    内,lo=mid+1=4,hi=6,mid=5,此时nums[lo]<nums[hi]且nums[mid]<nums[hi]
+ *    故此时nums[lo]即为所求解.
+ */
+int
+findMin(int *nums, int numsSize) {
+  int lo, hi, mid;
+
+  lo = 0, hi = numsSize - 1;
+  while (lo <= hi) {
+    mid = (lo + hi) / 2;
+    if (nums[lo] <= nums[mid] && nums[mid] <= nums[hi])
+      return nums[lo];
+    else if (nums[mid] > nums[hi])
+      lo = mid + 1;
+    else if (nums[lo] > nums[mid] && nums[mid] < nums[hi])
+      hi = mid;
+  }
+
+  return 0;
 }
