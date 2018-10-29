dd798d6628b7185d575c5e73c9a663f392e83661
Lancer Wong <lancer_cool@163.com>
2018-10-29 15:28:27 +0800

    补上最小堆的解决方法

--- kth_largest_element_in_an_array.c
+++ kth_largest_element_in_an_array.c
@@ -127,3 +127,101 @@ findKthLargest(int *nums, int numsSize, int k) {
 
   return nums[k];
 }
+
+
+/**
+ * 补上最小堆的解决方法.
+ */
+typedef struct Heap *Heap;
+
+struct Heap {
+  int capacity;
+  int size;
+  int *array;
+};
+
+Heap
+heapCreate(int maxsize) {
+  Heap heap;
+
+  heap = malloc(sizeof(struct Heap));
+  heap->capacity = maxsize;
+  heap->size = 0;
+  heap->array = malloc(maxsize * sizeof(int));
+
+  return heap;
+}
+
+/**
+ * 堆的插入,因为堆的性质,在插入时先在最后插入元素,然后将
+ * 此元素上滤.
+ */
+void
+heapInsert(Heap h, int key) {
+  int curr, parent;
+  int *ht = h->array;
+
+  curr = h->size++;
+
+  while (curr > 0) {
+    parent = (curr - 1) >> 1;
+    /**
+     * 最小堆,父节点大于子节点,子节点上滤.
+     * 此时并不需要交换两个节点的值.只需要
+     * 将父节点的值赋给子节点即可.最后循环
+     * 结束后的curr位置即插入的位置.
+     */
+    if (ht[parent] > key) {
+      ht[curr] = ht[parent];
+      curr = parent;
+    } else
+      break;
+  }
+  ht[curr] = key;
+}
+
+/**
+ * 寻找数组中的最大K个数,先构建一个K个元素的
+ * 最小堆,剩下的元素逐个校验:
+ * 1. 若小于等于堆顶的元素,则抛弃;
+ * 2. 若大于堆顶元素则替换掉堆顶元素,并下滤找到合适的位置.
+ */
+void
+pryDown(Heap h, int key) {
+  int curr = 0, left = 1, right = 2;
+  int *ht = h->array;
+
+  if (key <= ht[curr])
+    return;
+
+  /**
+   * 注意判断左右子节点是否超出堆的大小.
+   */
+  while ((left < h->capacity && key > ht[left])
+         || (right < h->capacity && key > ht[right])) {
+    if (right < h->capacity && ht[left] > ht[right]) {
+      ht[curr] = ht[right];
+      curr = right;
+    } else {
+      ht[curr] = ht[left];
+      curr = left;
+    }
+    left = (curr << 1) + 1;
+    right = (curr + 1) << 1;
+  }
+  ht[curr] = key;
+}
+
+int
+findKthLargest(int *nums, int numsSize, int k) {
+  Heap h;
+  int i;
+
+  h = heapCreate(k);
+  for (i = 0; i < k; ++i)
+    heapInsert(h, nums[i]);
+  while (i < numsSize)
+    pryDown(h, nums[i++]);
+
+  return h->array[0];
+}
