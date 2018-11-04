2b66b00d832232ff4a7f8c1c8d86bc1b0bb6d1c0
april01xxx <april01xxx@gmail.com>
2018-11-04 14:12:55 +0800

    summary ranges

---
+++ summary_ranges.c
@@ -0,0 +1,40 @@
+/**
+ * Given a sorted integer array without duplicates, return the summary of its
+ * ranges.
+ *
+ * Example 1:
+ * Input:  [0,1,2,4,5,7]
+ * Output: ["0->2","4->5","7"]
+ * Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+ *
+ * Example 2:
+ * Input:  [0,2,3,4,6,8,9]
+ * Output: ["0","2->4","6","8->9"]
+ * Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+ *
+ * 给定一个升序排列的数组,输出它的区间.
+ * 需要注意的是负数问题,故用一个char数组来处理.
+ */
+class Solution {
+public:
+  vector<string> summaryRanges(vector<int>& nums) {
+    vector<string> ans;
+    char s[20] = {0};
+
+    if (nums.size() == 0)
+      return ans;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      sprintf(s, "%d", nums[i]);
+      int j = i;
+      while (i < nums.size() - 1 && nums[i] + 1 == nums[i + 1])
+        ++i;
+      if (i > j) {
+        sprintf(s, "%s->%d", s, nums[i]);
+      }
+      ans.push_back(s);
+      memset(s, 0, 20);
+    }
+    return ans;
+  }
+};
