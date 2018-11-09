c80bcc635435803e5e6b5056886229b8ba3c2a19
Lancer Wong <lancer_cool@163.com>
2018-11-09 17:53:20 +0800

    missing number

---
+++ missing_number.c
@@ -0,0 +1,19 @@
+/**
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+ * find the one that is missing from the array.
+ * 
+ * Example 1:
+ * 
+ * Input: [3,0,1]
+ * Output: 2
+ * Example 2:
+ * 
+ * Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement 
+ * it using only constant extra space complexity?
+ *
+ * 这题和前面一道First Missing Positive的题很像,不过那道题因为元素可能大于N,所以
+ * 在思维上来说会很难想到.
+ */
